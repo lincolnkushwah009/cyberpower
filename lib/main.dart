e51528671bb80f81bdbb4ad182c73cf228b02759
sunydepalpur.2010@gmail.com <Anupama@98@1>
2020-07-30 14:14:57 +0530

    issue chk

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:cyberpower/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'config/AppConfig.dart';
 import 'login.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 
 //void main() => runApp(MyApp());
 //
@@ -22,30 +22,22 @@ import 'package:shared_preferences/shared_preferences.dart';
 //  }
 //}
 
-
-
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
-        statusBarColor: Colors.white, //top bar color
-        statusBarIconBrightness: Brightness.dark, //top bar icons
-        systemNavigationBarColor: Colors.white, //bottom bar color
-        systemNavigationBarIconBrightness: Brightness.dark, //bottom bar icons
-      )
-  );
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.white, //top bar color
+    statusBarIconBrightness: Brightness.dark, //top bar icons
+    systemNavigationBarColor: Colors.white, //bottom bar color
+    systemNavigationBarIconBrightness: Brightness.dark, //bottom bar icons
+  ));
   SharedPreferences prefs = await SharedPreferences.getInstance();
   var email = prefs.getString('emailAddress');
+  AppConfig.userID = prefs.getString('userid');
   print(email);
   SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
-      .then((_) => runApp(
-      MaterialApp(
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_) => runApp(MaterialApp(
           debugShowCheckedModeBanner: false,
-          home: email==null?LoginPage():Home())
-          )
-          );
+          home: email == null ? LoginPage() : Home())));
 }
-
